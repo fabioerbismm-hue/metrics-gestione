c056d984323ebe344b9edc1c263e8ab56714ba78
fabioerbismm-hue <fabioerbismm-hue@users.noreply.github.com>
2026-06-15 14:39:06 +0200

    Configure Supabase project

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 window.METRICS_CONFIG = {
-  supabaseUrl: "",
-  supabaseAnonKey: "",
+  supabaseUrl: "https://zonutshwipeqitzjlhkq.supabase.co",
+  supabaseAnonKey: "sb_publishable_GU-iA2SrmsPhfd0ZmNd_sQ_gjwNaO96",
 };
